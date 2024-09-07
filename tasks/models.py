3e71af11db2145867f8ff37045638a443307e724
Kirill <159899339+kirakorst@users.noreply.github.com>
2024-09-08 02:46:52 +0400

    8

--- tasks/models.py
+++ tasks/models.py
@@ -3,16 +3,21 @@ import datetime
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    status = models.CharField(max_length=20, choices=[
-        ('todo', 'To Do'),
-        ('in_progress', 'In Progress'),
-        ('done', 'Done'),
+    title = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    status = models.CharField(max_length=20, verbose_name="Статус", choices=[
+        ('todo', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Выполнено'),
     ], default='todo')
-    due_date = models.DateField(default=datetime.datetime.now(), null=True, blank=True)
+    due_date = models.DateField(default=datetime.datetime.now(),verbose_name="Дата", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+
+    class Meta:   #Склоняет имя модели в ед и мн числе в (админе)
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
 
     def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render,redirect
 from tasks.models import Task
 from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def task_list(request):
     tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
-
+@login_required
 def task_detail(request, pk):
     task = Task.objects.get(pk=pk)
     return render(request, 'tasks/task_detail.html', {'task': task})
-
+@login_required
 def task_create(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -20,7 +21,7 @@ def task_create(request):
     else:
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form})
-
+@login_required
 def task_update(request, pk):
     task = Task.objects.get(pk=pk)
     if request.method == 'POST':
@@ -31,7 +32,7 @@ def task_update(request, pk):
     else:
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_form.html', {'form': form})
-
+@login_required
 def task_delete(request, pk):
     task = Task.objects.get(pk=pk)
     if request.method == 'POST':
