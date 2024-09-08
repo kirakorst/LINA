1c8f66087c4f2b8fbb233e48c955ca52696d11ee
Kirill <159899339+kirakorst@users.noreply.github.com>
2024-09-08 23:21:28 +0400

    9

--- tasks/models.py
+++ tasks/models.py
@@ -6,10 +6,10 @@ class Task(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
     status = models.CharField(max_length=20, verbose_name="Статус", choices=[
-        ('todo', 'Новая'),
-        ('in_progress', 'В процессе'),
-        ('done', 'Выполнено'),
-    ], default='todo')
+        ('Новая', 'Новая'),
+        ('В процессе', 'В процессе'),
+        ('Выполнена', 'Выполнено'),
+    ], default='Новая')
     due_date = models.DateField(default=datetime.datetime.now(),verbose_name="Дата", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
