56addeec75aaa8cd79907be96a82124ec39c7ac6
Kirill <159899339+kirakorst@users.noreply.github.com>
2024-09-10 14:49:13 +0400

    12

--- storage/views.py
+++ storage/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UploadFileForm
 from .models import UploadedFile
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+import os
+from django.http import HttpResponse, Http404
 
 @login_required
 def upload_file(request):
@@ -14,18 +15,31 @@ def upload_file(request):
     else:
         form = UploadFileForm()
     return render(request, 'storage/upload_file.html', {'form': form})
+
 @login_required
 def list_files(request):
     files = UploadedFile.objects.all()
     return render(request, 'storage/list_files.html', {'files': files})
+
 @login_required
 def download_file(request, id):
-    file = UploadedFile.objects.get(id=id)
-    response = HttpResponse(file.file.read())
-    response['Content-Disposition'] = f'attachment; filename="{file.title}"'
-    return response
+    # Получаем объект UploadedFile по id
+    uploaded_file = get_object_or_404(UploadedFile, id=id)
+    file_path = uploaded_file.file.path  # Получаем полный путь к файлу
+
+    if not os.path.exists(file_path):
+        raise Http404("File does not exist")
+
+    try:
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/octet-stream")
+            response['Content-Disposition'] = f'attachment; filename={os.path.basename(file_path)}'
+            return response
+    except IOError:
+        raise Http404("File does not exist")
+
 @login_required
 def delete_file(request, id):
-    file = UploadedFile.objects.get(id=id)
+    file = get_object_or_404(UploadedFile, id=id)
     file.delete()
     return redirect('list_files')
