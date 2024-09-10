52445a8ffaa0e9f831f4bf0b1eabaa67c2a3b8c1
Kirill <159899339+kirakorst@users.noreply.github.com>
2024-09-10 16:25:08 +0400

    13

--- storage/models.py
+++ storage/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.conf import settings
+
+
 
 class UploadedFile(models.Model):
     title = models.CharField(max_length=100)
     file = models.FileField(upload_to='files/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
