b18e572592d0ad8f6652506db6f0b7d3d9901a40
Kirill <159899339+kirakorst@users.noreply.github.com>
2024-09-10 16:43:41 +0400

    14

--- storage/views.py
+++ storage/views.py
@@ -10,7 +10,9 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
             return redirect('list_files')  # Перенаправление после успешной загрузки
     else:
         form = UploadFileForm()
@@ -18,7 +20,7 @@ def upload_file(request):
 
 @login_required
 def list_files(request):
-    files = UploadedFile.objects.all()
+    files = UploadedFile.objects.filter(user=request.user)
     return render(request, 'storage/list_files.html', {'files': files})
 
 @login_required
@@ -26,6 +28,11 @@ def download_file(request, id):
     # Получаем объект UploadedFile по id
     uploaded_file = get_object_or_404(UploadedFile, id=id)
     file_path = uploaded_file.file.path  # Получаем полный путь к файлу
+
+    if uploaded_file.user != request.user:
+        raise Http404("Access denied")
+
+    file_path = uploaded_file.file.path
 
     if not os.path.exists(file_path):
         raise Http404("File does not exist")
@@ -41,5 +48,8 @@ def download_file(request, id):
 @login_required
 def delete_file(request, id):
     file = get_object_or_404(UploadedFile, id=id)
-    file.delete()
-    return redirect('list_files')
+    if file.user == request.user:
+        file.delete()
+        return redirect('list_files')
+    else:
+        raise Http404("Access denied")
