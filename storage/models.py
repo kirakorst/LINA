d5c6692ee8ece34752096cd3977eb38fe2898a4e
Kirill <159899339+kirakorst@users.noreply.github.com>
2024-09-16 17:33:43 +0400

    28

--- storage/models.py
+++ storage/models.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 
 class UploadedFile(models.Model):
-    title = models.CharField(max_length=100)
-    file = models.FileField(upload_to='files/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100, verbose_name="Название")
+    file = models.FileField(upload_to='files/', verbose_name="Файл")
+    uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата загрузки")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Пользователь")
 
     def __str__(self):
         return self.title
